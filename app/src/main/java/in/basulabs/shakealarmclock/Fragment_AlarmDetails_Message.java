aaa5e2de2c68b028d28a502385fb0f6c70b51757
Wrichik Basu <56736644+WrichikBasu@users.noreply.github.com>
2021-01-26 17:13:48 +0530

    Implemented alarm message user input
    The user will now be able to type and save an alarm message. The message will not yet be displayed; that will be implemented in the next commit.

---
+++ app/src/main/java/in/basulabs/shakealarmclock/Fragment_AlarmDetails_Message.java
@@ -0,0 +1,60 @@
+package in.basulabs.shakealarmclock;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class Fragment_AlarmDetails_Message extends Fragment {
+
+    private ViewModel_AlarmDetails viewModel;
+
+    private EditText messageEditText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.frag_alarmdetails_message, container, false);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(ViewModel_AlarmDetails.class);
+
+        messageEditText = view.findViewById(R.id.alarmMessageEditText);
+
+        if (viewModel.getAlarmMessage() == null){
+            messageEditText.setText("");
+        } else {
+            messageEditText.setText(viewModel.getAlarmMessage());
+        }
+
+        messageEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() == 0){
+                    viewModel.setAlarmMessage(null);
+                } else {
+                    viewModel.setAlarmMessage(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        return view;
+    }
+}
